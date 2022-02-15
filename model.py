7ccf169b1b05ba0b51753493ee0dc79ba4958ccb
ptran1203 <phatth1203@gmail.com>
2022-02-15 10:01:03 +0700

    param

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import timm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+
 
 def gem(x, p=3, eps=1e-6):
     return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
