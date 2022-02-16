d46b62c05cc07216f711db2e0f7dfaa5075d59ca
ptran1203 <phatth1203@gmail.com>
2022-02-16 10:46:49 +0700

    P non trainable

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ from torch.nn.parameter import Parameter
 def gem(x, p=3, eps=1e-6):
     return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
 class GeM(nn.Module):
-    def __init__(self, p=3, eps=1e-6, p_trainable=True):
+    def __init__(self, p=3, eps=1e-6, p_trainable=False):
         super(GeM,self).__init__()
         if p_trainable:
             self.p = Parameter(torch.ones(1)*p)
