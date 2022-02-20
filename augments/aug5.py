2f498de80001744ea49968ab7ea42cb8b82a567d
ptran1203 <phatth1203@gmail.com>
2022-02-20 12:15:45 +0700

    baseline

--- augments/aug5.py
+++ augments/aug5.py
@@ -5,13 +5,13 @@ def train_transform(img_size):
         A.SmallestMaxSize(img_size),
         A.RandomCrop(img_size, img_size, p=1.0),
         A.HorizontalFlip(p=0.5),
-        A.ImageCompression(quality_lower=99, quality_upper=100, p=0.1),
-        A.GaussNoise(p=0.1),
-        A.GaussianBlur(blur_limit=3, p=0.1),
-        A.RandomBrightnessContrast(p=0.5, brightness_limit=(-0.2, 0.2), contrast_limit=(-0.2, 0.2)),
-        A.HueSaturationValue(p=0.5, hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2),
+        # A.ImageCompression(quality_lower=99, quality_upper=100, p=0.1),
+        # A.GaussNoise(p=0.1),
+        # A.GaussianBlur(blur_limit=3, p=0.1),
+        A.RandomBrightnessContrast(p=0.75, brightness_limit=0.1, contrast_limit=0.1),
+        A.HueSaturationValue(p=0.75, hue_shift_limit=0.01, sat_shift_limit=0.3, val_shift_limit=0.2),
         A.Cutout(p=0.5, num_holes=1, max_h_size=int(img_size * 0.2), max_w_size=int(img_size * 0.2)),
-        A.ShiftScaleRotate(p=0.75, shift_limit=0.1, scale_limit=0.2, rotate_limit=10, border_mode=0),
+        # A.ShiftScaleRotate(p=0.75, shift_limit=0.1, scale_limit=0.2, rotate_limit=10, border_mode=0),
         A.Normalize()
     ])
 
