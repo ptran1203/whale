df94428e56cdf3070f7696d1bc7c34381e21ed45
ptran1203 <phatth1203@gmail.com>
2022-02-23 11:05:30 +0700

    adam

--- train.py
+++ train.py
@@ -99,8 +99,8 @@ def main(args):
     model = Net(args.backbone, n_classes, cfg=args, pretrained=True)
     # print(model)
 
-    optimizer = optim.SGD(model.parameters(), lr=args.init_lr, weight_decay=1e-4, momentum=0.9, nesterov=True)
-    # optimizer = optim.Adam(model.parameters(), lr=args.init_lr, weight_decay=1e-4)
+    # optimizer = optim.SGD(model.parameters(), lr=args.init_lr, weight_decay=1e-4, momentum=0.9, nesterov=True)
+    optimizer = optim.Adam(model.parameters(), lr=args.init_lr, weight_decay=5e-4)
     # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs)
     num_train_steps = len(train_loader)
     # print('Training steps:', num_train_steps)
