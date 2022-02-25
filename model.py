2ea0e76ee90804c49f5d723d979110dbe5a1e3f1
ptran1203 <phatth1203@gmail.com>
2022-02-25 12:38:56 +0700

    dim0

--- model.py
+++ model.py
@@ -101,7 +101,7 @@ class ArcMarginProduct(nn.Module):
 
     def forward(self, input, label):
         # --------------------------- cos(theta) & phi(theta) ---------------------------
-        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+        cosine = F.linear(F.normalize(input), F.normalize(self.weight, dim=0))
         sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
         phi = cosine * self.cos_m - sine * self.sin_m
         if self.easy_margin:
