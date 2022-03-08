5428dbe4dab2589b0eed6f2060ffab5627ab9c47
ptran1203 <phatth1203@gmail.com>
2022-03-08 16:32:33 +0700

    weight_decay

--- tf/model.py
+++ tf/model.py
@@ -257,7 +257,7 @@ def get_model_embed(config, strategy):
             embed = tfhub.KerasLayer(hub_url, trainable=True)(inp)
             
         embed = tf.keras.layers.Dropout(0.3)(embed)
-        embed = tf.keras.layers.Dense(512)(embed)
+        embed = tf.keras.layers.Dense(512, kernel_regularizer=tf.keras.regularizers.l2(1e-4))(embed)
         #embed = tf.keras.layers.BatchNormalization()(embed)
         x = margin([embed, label])
         
