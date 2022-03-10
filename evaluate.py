cb2ae9cd4ee396d8a47b81722ff8ec56afc13157
ptran1203 <phatth1203@gmail.com>
2022-03-10 16:34:16 +0700

    new_compute_sim

--- evaluate.py
+++ evaluate.py
@@ -71,6 +71,50 @@ def compute_sim(train_df, train_embs, test_embs, thr=0.65, norm=False):
     sim_df = pd.DataFrame(records, columns=['image', 'predictions'])
     return sim_df
 
+
+def compute_simv2(train_df, train_embs, test_embs, thr=0.65, norm=False):
+    # Compute center of each individual id
+    label2emb = defaultdict(list)
+    for label, d in train_df.groupby('individual_id'):
+        for img_id in d.image.values:
+            if img_id in train_embs:
+                label2emb[label].append(train_embs[img_id])
+
+    for k, v in label2emb.items():
+        avg = np.mean(np.stack(v), 0)
+        label2emb[k] = avg / np.linalg.norm(avg)
+
+    train_k, train_v = dict2list(label2emb)
+    test_k, test_v = dict2list(test_embs)
+
+    if norm:
+        train_v = l2norm_numpy(train_v)
+        test_v = l2norm_numpy(test_v)
+    else:
+        print("[WARN] You should use norm to apply threshold correctly")
+
+    cos = np.dot(test_v, train_v.T)
+
+    records = []
+
+    for i, scores in enumerate(tqdm(cos)):
+        sort_idx = np.argsort(scores)[::-1]
+        top5 = [train_k[j] for j in sort_idx[:5]]
+        # top5 = [train_map[x] for x in top5]
+
+        if thr > 0.0:
+            for j in range(5):
+                if scores[sort_idx[j]] < thr:
+                    top5 = top5[:j] + ['new_individual'] + top5[j:4]
+                    break
+        
+        # print(top5, scores[sort_idx][:5])
+        # print(test_k[i], [f"{train_k[j]}({scores[j]:.3f})" for j in sort_idx[:5]])
+        # print(scores[-1])
+        records.append([test_k[i], " ".join(top5)])
+    sim_df = pd.DataFrame(records, columns=['image', 'predictions'])
+    return sim_df
+
 def evaluate(val_df, train_embs, val_embs, norm=False):
     sim_df = compute_sim(val_df, train_embs, val_embs, thr=1.0, norm=norm)
 
