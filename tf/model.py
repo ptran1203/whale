4531bc8aa83b9fef9c9e9b4ad7b876cf657b58a3
ptran1203 <phatth1203@gmail.com>
2022-03-12 16:25:52 +0700

    morebn

--- tf/model.py
+++ tf/model.py
@@ -262,7 +262,9 @@ def get_model_embed(config, strategy):
                 embed = tfhub.KerasLayer(hub_url, trainable=True)(inp)
             
         embed = tf.keras.layers.Dropout(0.5)(embed)
+        embed = tf.keras.layers.BatchNormalization()(embed)
         embed = tf.keras.layers.Dense(512, kernel_regularizer=tf.keras.regularizers.l2(1e-4), use_bias=False)(embed)
+        embed = tf.keras.layers.BatchNormalization()(embed)
         x = margin([embed, label])
         
         output = tf.keras.layers.Softmax(dtype='float32')(x)
