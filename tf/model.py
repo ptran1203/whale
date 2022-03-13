44e150e16b3ca0ec7395cce28486f7c07b8eebdd
ptran1203 <phatth1203@gmail.com>
2022-03-13 21:44:14 +0700

    fix

--- tf/model.py
+++ tf/model.py
@@ -249,6 +249,10 @@ def get_model_embed(config, strategy):
             import tfimm
             
             x = tfimm.create_model(config.model_type, pretrained="timm")(inp)
+            if config.pool == 'avg':
+                embed = tf.keras.layers.GlobalAveragePooling2D()(x)
+            else:
+                embed = GeM()(x)
         else:
             if config.model_type == 'effnetv1':
                 x = EFNS[config.EFF_NET](weights='noisy-student', include_top=False)(inp)
