6903ae522dcfe033accfabdbced80e52905949c0
ptran1203 <phatth1203@gmail.com>
2022-03-15 23:12:33 +0700

    focal

--- tf/model.py
+++ tf/model.py
@@ -12,62 +12,10 @@ from tensorflow.keras import backend as K
 import json
 import tensorflow_hub as tfhub
 import tensorflow_addons as tfa
+from tf.losses import SparseCategoricalFocalLoss
 from datetime import datetime
 
 n_species = 26
-
-def focal_loss(gamma=2., alpha=1.0):
-
-    gamma = float(gamma)
-    alpha = float(alpha)
-
-    def focal_loss_fixed(y_true, y_pred):
-        """Focal loss for multi-classification
-        FL(p_t)=-alpha(1-p_t)^{gamma}ln(p_t)
-        Notice: y_pred is probability after softmax
-        gradient is d(Fl)/d(p_t) not d(Fl)/d(x) as described in paper
-        d(Fl)/d(p_t) * [p_t(1-p_t)] = d(Fl)/d(x)
-        Focal Loss for Dense Object Detection
-        https://arxiv.org/abs/1708.02002
-
-        Arguments:
-            y_true {tensor} -- ground truth labels, shape of [batch_size, num_cls]
-            y_pred {tensor} -- model's output, shape of [batch_size, num_cls]
-
-        Keyword Arguments:
-            gamma {float} -- (default: {2.0})
-            alpha {float} -- (default: {4.0})
-
-        Returns:
-            [tensor] -- loss.
-        """
-        epsilon = 1.e-9
-        y_true = tf.one_hot(y_true, depth=y_pred.shape[1])
-        y_true = tf.convert_to_tensor(y_true, tf.float32)
-        y_pred = tf.convert_to_tensor(y_pred, tf.float32)
-
-        model_out = tf.add(y_pred, epsilon)
-        ce = tf.multiply(y_true, -tf.math.log(model_out))
-        weight = tf.multiply(y_true, tf.pow(tf.subtract(1., model_out), gamma))
-        fl = tf.multiply(alpha, tf.multiply(weight, ce))
-        reduced_fl = tf.reduce_max(fl, axis=1)
-        return tf.reduce_mean(reduced_fl)
-    return focal_loss_fixed
-
-def focal_loss_softmax(labels, y_pred, gamma=2):
-    """
-    Computer focal loss for multi classification
-    Args:
-      labels: A int32 tensor of shape [batch_size].
-      logits: A float32 tensor of shape [batch_size,num_classes].
-      gamma: A scalar for focal loss gamma hyper-parameter.
-    Returns:
-      A tensor of the same shape as `lables`
-    """
-    labels=tf.one_hot(labels, depth=y_pred.shape[1])
-    L=-labels*((1-y_pred)**gamma)*tf.math.log(y_pred)
-    L=tf.reduce_sum(L,axis=1)
-    return L
 
 class ArcMarginProduct(tf.keras.layers.Layer):
     '''
@@ -341,7 +289,7 @@ def get_model_embed(config, strategy):
         if config.loss == 'ce':
             loss_func = tf.keras.losses.SparseCategoricalCrossentropy()
         elif config.loss == 'focal':
-            loss_func = focal_loss_softmax
+            loss_func = SparseCategoricalFocalLoss(gamma=2.0)
         else:
             raise ValueError(config.loss)
 
