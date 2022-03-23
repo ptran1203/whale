83d6d6e87fb77734d7f67b5e4606da9837d7f9aa
ptran1203 <phatth1203@gmail.com>
2022-03-23 15:44:35 +0700

    cutout

--- tf_code/data.py
+++ tf_code/data.py
@@ -58,9 +58,9 @@ def data_augment(config, posting_id, image, label_group, matches):
         if config.CUTOUT:
             N_CUTOUT = 1
             for cutouts in range(N_CUTOUT):
-                if tf.random.uniform([]) <= 0.2:
+                if tf.random.uniform([]) <= 0.5:
                     DIM = config.IMAGE_SIZE
-                    CUTOUT_LENGTH = DIM//8
+                    CUTOUT_LENGTH = DIM//6
                     x1 = tf.cast( tf.random.uniform([],0,DIM-CUTOUT_LENGTH),tf.int32)
                     x2 = tf.cast( tf.random.uniform([],0,DIM-CUTOUT_LENGTH),tf.int32)
                     filter_ = tf.concat([tf.zeros((x1,CUTOUT_LENGTH)),tf.ones((CUTOUT_LENGTH,CUTOUT_LENGTH)),tf.zeros((DIM-x1-CUTOUT_LENGTH,CUTOUT_LENGTH))],axis=0)
